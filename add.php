55e97bc94f7457109c7c6b3ba9105026fc7776b2
samrinamim <ssmim.phys@gmail.com>
2015-01-01 16:07:17 +0600

    Uploading Checkbox............

--- add.php
+++ add.php
@@ -7,6 +7,8 @@ echo '<pre>';
 
 $Fullname = $_POST['full_name'];
 
+/*
+
 if (isset($_POST['hobby_reading']) && !empty($_POST['hobby_reading'])) {
 
     $Hobby .=$_POST['hobby_reading'].',';
@@ -32,12 +34,62 @@ $Hobby = rtrim($Hobby, ',');
 //$Hobby = substr($Hobby,0,-1);
 
 echo $Hobby;
-exit;
+
+*/
+$Hobby = '';
+
+
+if((array_key_exists('hobby_reading',$_POST)&& !empty($_POST['hobby_reading'])))
+{
+ $Hobby .= $_POST['hobby_reading'].',';
+}
+
+if((array_key_exists('hobby_playing',$_POST)&& !empty($_POST['hobby_playing'])))
+{
+    $Hobby .= $_POST['hobby_playing'].',';
+}
+if((array_key_exists('hobby_gardening',$_POST)&& !empty($_POST['hobby_gardening'])))
+{
+    $Hobby .= $_POST['hobby_gardening'].',';
+}
+if((array_key_exists('hobby_travelling',$_POST)&& !empty($_POST['hobby_travelling'])))
+{
+    $Hobby .= $_POST['hobby_travelling'].',';
+}
+
+$Hobby = rtrim($Hobby, ',');
+//$Hobby = substr($Hobby,0,-1);
+
+echo $Hobby;
+
+
+echo "<br>";
+echo "<br>";
 
 $Food = $_POST['food'];
+
+$Food = implode(",",$Food);
+
+echo $Food;
+
+echo "<br>";
+echo "<br>";
+
 $Joblocation = $_POST['job_location'];
+
+
+$Joblocation = implode(",",$Joblocation);
+
+echo $Joblocation;
+
+
+
 $Created = $_POST['created_date'];
 $Modified = $_POST['modified_date'];
+
+
+/*
+echo
 
 var_dump($Hobby_reading);
 var_dump($Hobby_playing);
@@ -47,14 +99,14 @@ var_dump($Hobby_travelling);
 var_dump($Joblocation);
 
 //echo $birth_day;
-
+*/
 $link = mysqli_connect("localhost",
     "root",
     "mysql123",
     "multiple") or die('cannot connect');
 $query = "INSERT INTO `multiple`.`information` (`id`,
-`full_name`,`hobby`,`job_location`,`created_date`,`modified_date`)
-VALUES (NULL ,'$Fullname', '$Hobby','$Joblocation',NOW(),'')";
+`full_name`,`hobby`,`food`,`job_location`,`created_date`,`modified_date`)
+VALUES (NULL ,'$Fullname', '$Hobby','$Food','$Joblocation',NOW(),'')";
 //echo $query;
 mysqli_query($link, $query);
 //header('location:list.php');
